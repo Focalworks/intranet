a31f6b4dd496ded75c2e5e1c0a9a4652d336f42b
Amitav Roy <reachme@amitavroy.com>
2014-10-22 12:45:40 +0530

    Adding the events to send out emails

---
+++ workbench/focalworks/mailing/src/views/emails/new-grievance-email.blade.php
@@ -0,0 +1,6 @@
+<h1>A new Grievance was added into the Intranet.</h1>
+
+<p><strong>Title:</strong>: {{$grievance['title']}}</p>
+
+<p><strong>Description:</strong>:</p>
+<p>{{$grievance['description']}}</p>
