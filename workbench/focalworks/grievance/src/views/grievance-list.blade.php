d065a5245251741c9c9185ef29444f7bf42f69e8
Urmila <urmila.patel@foalworks.in>
2014-11-03 17:21:02 +0530

    add new in div

--- workbench/focalworks/grievance/src/views/grievance-list.blade.php
+++ workbench/focalworks/grievance/src/views/grievance-list.blade.php
@@ -1,10 +1,15 @@
 @section('scripts')
 @parent
+<script src="//cdn.ckeditor.com/4.4.3/basic/ckeditor.js"></script>
 <script type="text/javascript">
 $(function() {
     $('.mytest').tooltip();
     $('.confirm').click(function(){
         return confirm("Sure you want to delete ?");
+    });
+    $( "#addnew" ).click(function() {
+        $( "#div_addnew" ).show();
+         $( "#addnew" ).hide();
     });
 });
 </script>
@@ -15,8 +20,81 @@ $(function() {
 </div>
 <div class="cards-view">
 <div class="row">
-    <div class="col-md-12">
-        <p><a href="{{url('grievance/add')}}" class="btn btn-success btn-md"><span class="glyphicon glyphicon-plus"></span>&nbsp;ADD NEW</a></p>
+    <div class="col-md-12 ">
+        <button id="addnew" class="btn btn-success btn-md"><span class="glyphicon glyphicon-plus"></span>&nbsp;ADD NEW</button>
+    </div>
+    <div style="display: none" id="div_addnew" class="col-md-12 ">
+        <?php
+$urgency = Input::old('urgency');
+if ($urgency == '') {
+    $urgency = 1;
+}
+?>
+
+{{ Form::open(array('url' => 'grievance/save', 'role' => 'form', 'files' => true)) }}
+<div class="row">
+    <div class="col-lg-4 col-md-5">
+        <h2>Add Grievance</h2>
+    </div>
+    <div class="col-lg-4 col-md-5">
+        <div class="btn-wrap">
+            <button type="submit" class="btn btn-success btn-md"><span class="glyphicon glyphicon-floppy-save"></span>&nbsp; Save</button>
+        </div>
+    </div>
+</div>
+<div class="row">
+        <div class="col-lg-8 col-md-10">
+            <div class="cards-view">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" class="form-control" id="title" placeholder="" name="title" value="{{Input::old('title')}}">
+                            <span class="error-display">{{$errors->first('title')}}</span>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="body">Body</label>
+                            <textarea name="body" id="body" class="form-control">{{Input::old('body')}}</textarea>
+                            <span class="error-display">{{$errors->first('body')}}</span>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="category">Category</label>
+                            {{GlobalHelper::getDropdownFromArray('category', Grievance::getGrievanceCategories(), Input::old('category'))}}
+                            <span class="error-display">{{$errors->first('category')}}</span>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="urgency">Urgency</label>
+                            {{GlobalHelper::getDropdownFromArray('urgency', Grievance::getUrgencies(), $urgency)}}
+                            <span class="error-display">{{$errors->first('urgency')}}</span>
+                        </div>
+                    
+                        <div class="form-group">
+                            <label for="urgency">Anonymous</label> &nbsp;
+                            <input type="checkbox" name="anonymous" id="anonymous" value="1" >
+                        </div>   
+                        <div class="form-group upload-img-wrapper">
+                            <label for="photo">Photo if any</label>
+                            <div class="upload-img">
+                                <input type="file" class="form-input custom-file-input" placeholder="Photo" name="photo">
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+{{ Form::close() }}
+<script>
+    $(window).load(function ()
+    {
+        CKEDITOR.replace('body');
+    });
+</script>
     </div>
 </div>
 @if ($grievanceCount <= 0)
