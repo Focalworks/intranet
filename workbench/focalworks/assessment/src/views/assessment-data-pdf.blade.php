89e331f2aa73c4eefd4b5fb59cd069ca4d76f0b5
Snehal Salvi <snehal.salvi@focalworks.in>
2014-11-04 12:27:03 +0530

    Print PDF styling

--- workbench/focalworks/assessment/src/views/assessment-data-pdf.blade.php
+++ workbench/focalworks/assessment/src/views/assessment-data-pdf.blade.php
@@ -7,20 +7,41 @@
     body, h1, h2, h3, h4, h5 {
         font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
     }
+    .container {
+        border:10px solid #457fbf;
+        padding: 20px;
+    }
+    table {
+        width: 100%;
+         page-break-inside:auto 
+    }
+    table tr td{
+        border: 1px solid #cccccc;
+        padding: 5px;
+        width: 50%;
+    }
+ 
+    .title {
+        background: #edf6ff;
+    }
+    h1 {
+        margin: 10px 0;
+        color: #457fbf;
+    } 
 </style>
 </head>
 <body class="container">
     <div class="row">
         <div class="col-md-12">
             <h1>User details</h1>
-                <table class="table table-bordered">
+                <table class="table table-bordered" cellpadding="0" cellspacing="0">
                     <tr>
-                        <td colspan="2"><strong>{{$data['name']}}</strong></td>
+                        <td colspan="2" class="title"><strong>{{$data['name']}}</strong></td>
                     </tr>
                     <tr>
                         <td><strong>Phone number</strong></td>
                         <td>{{$data['phone']}}</td>
-                    </tr>
+                    </tr> 
                     <tr>
                         <td><strong>Email</strong></td>
                         <td>{{$data['email']}}</td>
@@ -30,29 +51,32 @@
                         <td>{{$data['post_applied']}}</td>
                     </tr>
                 </table>
+                <br>
                 <h1>Assessment result</h1>
                 @foreach($data['question_data'] as $q_key => $question)
-                <table class="table table-striped table-bordered">
-                    <tr>
-                        <td colspan="2"><strong>{{$question['question']}}</strong></td>
-                    </tr>
-                    @foreach($question['option'] as $o_key =>$option)
-                    <tr>
-                        @if($question['correct'] == $option)
-                        <td width="50%"><strong>{{$option}}</strong></td>
-                        @else
-                        <td width="50%">{{$option}}</td>
-                        @endif
-                        <td width="50%">
-                            @if($data['user_result'][$q_key]['option_select'] == $o_key)
-                            User response
+                <div style="page-break-inside: avoid;">
+                    <table class="table table-striped table-bordered" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td colspan="2" class="title"><strong>{{$question['question']}}</strong></td>
+                        </tr>
+                        @foreach($question['option'] as $o_key =>$option)
+                        <tr>
+                            @if($question['correct'] == $option)
+                            <td width="50%"><strong>{{$option}}</strong></td>
                             @else
-                            &nbsp;
+                            <td width="50%">{{$option}}</td>
                             @endif
-                        </td>
-                    </tr>
-                    @endforeach
-                </table>
+                            <td width="50%">
+                                @if($data['user_result'][$q_key]['option_select'] == $o_key)
+                                User response
+                                @else
+                                &nbsp;
+                                @endif
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                </div>
                 <br/>
                 @endforeach
         </div>
